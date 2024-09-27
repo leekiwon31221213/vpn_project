6999614e639b4a25dd208c26319ed04909bce747
Lee Ki Won <cute01235@nate.com>
2024-09-27 14:48:35 +0900

    [update] 회원가입까지 완료

---
+++ js/scroll.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+  window.addEventListener('scroll', function () {
+    const main = document.querySelector('.header-main-container').clientHeight;
+    const section1_height = document.querySelector('.section1').clientHeight;
+    const section2_active = document.querySelector('.section2__inner');
+    const section2_li_active = document.querySelectorAll('.section2-contents > li');
+
+    if (window.scrollY >= main + section1_height / 2) {
+      if (!section2_active.classList.contains('active')) {
+        section2_active.classList.add('active');
+
+        section2_li_active.forEach((item, index) => {
+          setTimeout(() => {
+            item.classList.add('active');
+          }, index * 850);
+        });
+      }
+    }
+  });
+});
